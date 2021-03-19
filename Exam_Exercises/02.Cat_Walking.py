734ed74e32f4f687b3503c19d1cdf977a4faa2d3
svetoslava <svetoslava_stoyanova92@abv.bg>
2021-03-19 13:43:57 +0200

    new files

---
+++ Exam_Exercises/02.Cat_Walking.py
@@ -0,0 +1,9 @@
+n = int(input())
+m = int(input())
+s = int(input())
+
+for a in range(m, n + 1, -1):
+    if a % 2 == 0 and a % 3 == 0:
+        if a == s:
+            break
+        print(a, end=" ")
